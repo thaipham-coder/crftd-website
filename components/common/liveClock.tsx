3a11050b08b843af014ac8c988ad100941fd4b76
olavsajtos <o.sajtos@outlook.com>
2023-05-23 00:24:36 +0200

    Removed warnings from LiveClock

--- components/common/liveClock.tsx
+++ components/common/liveClock.tsx
@@ -1,19 +1,26 @@
-'use client'
-
-import Clock from 'react-live-clock'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
 
 /**
  * A component that displays a live clock with the current time in the 'Europe/Amsterdam' timezone.
  * The clock continuously updates and shows the time in the 'LT' format (e.g., 9:30 PM).
  */
 
+const DynamicClock = dynamic(() => import('react-live-clock'), { ssr: false })
+
 export default function LiveClock() {
-  return (
-    <Clock
-      format={'LT'}
-      ticking={true}
-      timezone={'Europe/Amsterdam'}
-      className="lowercase"
-    />
-  )
+    const [shouldRender, setShouldRender] = useState(false)
+
+    useEffect(() => {
+        setShouldRender(true)
+    }, [])
+
+    return shouldRender ? (
+        <DynamicClock
+            format={'LT'}
+            ticking={true}
+            timezone={'Europe/Amsterdam'}
+            className="lowercase"
+        />
+    ) : null
 }
