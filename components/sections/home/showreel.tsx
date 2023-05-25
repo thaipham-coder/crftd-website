9eed966c9edb3dfe590082c3931552e09c2842cb
olavsajtos <o.sajtos@outlook.com>
2023-05-26 00:41:38 +0200

    Edited scrolltrigger logic for showreel

--- components/sections/home/showreel.tsx
+++ components/sections/home/showreel.tsx
@@ -10,12 +10,13 @@ export default function Showreel() {
   useIsomorphicLayoutEffect(() => {
     const ctx = gsap.context(() => {
       gsap.from(el.current, {
-        clipPath: 'inset(0 40% 40% 40%)',
+        scale: 0.4,
+        transformOrigin: 'top center',
         scrollTrigger: {
           trigger: el.current,
           start: 'top 75%',
-          end: 'bottom 75%',
-          scrub: 1,
+          end: 'bottom 25%',
+          scrub: 1.5,
         },
       })
     }, el)
@@ -23,7 +24,7 @@ export default function Showreel() {
   }, [])
 
   return (
-    <section ref={el} className="aspect-video">
+    <section ref={el} className="clipped-corners aspect-video">
       <iframe
         src="https://player.vimeo.com/video/829920535?controls=0&autoplay=1&loop=1&muted=1&title=0&autopause=0&background=1"
         className="pointer-events-none h-full w-full"
