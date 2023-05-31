949a2416001cf9e66dcf28e3af6f50bb2c73266b
olavsajtos <o.sajtos@outlook.com>
2023-05-31 19:37:22 +0200

    Added qr logic, altered clock

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation';
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 
 export default function Qr() {
-    const router = useRouter();
+  const router = useRouter()
 
-    useEffect(() => {
-        window.location.href = 'https://www.crftd.nl/vcards/olavsajtos-vcard.vcf'
-        setTimeout(() => {
-            router.push('/')
-        }, 1000)
-    }, []);
-    return null;
+  useEffect(() => {
+    window.location.href = 'https://www.crftd.nl/vcards/olavsajtos-vcard.vcf'
+    setTimeout(() => {
+      router.push('/')
+    }, 1000)
+  }, [])
+  return null
 }
--- components/common/liveClock.tsx
+++ components/common/liveClock.tsx
@@ -1,26 +1,17 @@
-import dynamic from 'next/dynamic'
-import { useEffect, useState } from 'react'
+import Clock from 'react-live-clock'
 
 /**
  * A component that displays a live clock with the current time in the 'Europe/Amsterdam' timezone.
  * The clock continuously updates and shows the time in the 'LT' format (e.g., 9:30 PM).
  */
 
-const DynamicClock = dynamic(() => import('react-live-clock'), { ssr: false })
-
 export default function LiveClock() {
-  const [shouldRender, setShouldRender] = useState(false)
-
-  useEffect(() => {
-    setShouldRender(true)
-  }, [])
-
-  return shouldRender ? (
-    <DynamicClock
-      format={'LT'}
+  return (
+    <Clock
+      format="LT"
       ticking={true}
       timezone={'Europe/Amsterdam'}
       className="lowercase"
     />
-  ) : null
+  )
 }
