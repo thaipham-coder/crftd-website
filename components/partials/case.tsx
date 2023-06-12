a50648a96873e51477a318fb14b982829a980efb
olavsajtos <o.sajtos@outlook.com>
2023-06-12 18:38:03 +0200

    Added animations to cases on homepage

--- components/partials/case.tsx
+++ components/partials/case.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import Image, { StaticImageData } from 'next/image'
 import Link from 'next/link'
+import { gsap } from 'gsap'
+import useIsomorphicLayoutEffect from '@/components/helpers/isomorphicEffect'
+import { useRef } from 'react'
 
 export default function Case(props: {
   title: string
@@ -8,8 +13,54 @@ export default function Case(props: {
   src: StaticImageData
   href: string
 }) {
+  const el = useRef<HTMLAnchorElement>(null)
+
+  useIsomorphicLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(el.current, {
+        yPercent: 40,
+        scrollTrigger: {
+          trigger: el.current,
+          start: 'top-=100% bottom',
+          end: 'top 40%',
+          scrub: 1.5,
+        },
+      })
+
+      let hoverAnimation = gsap.timeline().fromTo(
+        el.current,
+        {
+          clipPath:
+            'polygon(0rem 0%, 100% 0%, 100% calc(100% - 0rem), calc(100% - 0rem) 100%, 0% 100%, 0% 0rem)',
+        },
+        {
+          scale: 0.96,
+          clipPath:
+            'polygon(6rem 0%, 100% 0%, 100% calc(100% - 6rem), calc(100% - 6rem) 100%, 0% 100%, 0% 6rem)',
+          ease: 'power3.out',
+          duration: 0.6,
+        },
+      )
+
+      if (el.current) {
+        el.current.addEventListener('mouseenter', () => {
+          if (hoverAnimation) {
+            hoverAnimation.play()
+          }
+        })
+        el.current.addEventListener('mouseleave', () => {
+          if (hoverAnimation) {
+            hoverAnimation.reverse()
+          }
+        })
+      }
+    }, el)
+    return () => ctx.revert()
+  }, [])
+
   return (
     <Link
+      ref={el}
       href={`/cases${props.href}`}
       id={`${props.title.toLowerCase()}-case`}
       data-type="link"
